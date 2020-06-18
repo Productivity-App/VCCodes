6d4a62df9198f8ba2e2d37d1c86245c9e6c0fd8c
Shiva <shiva_vasudevan@utexas.edu>
2020-06-18 15:01:32 -0500

    hello

---
+++ myvscodetest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def main():
+    
+    print("Hello, World!")
+    print("What is your name?")
+    print("I made it work!")
+    print("woohoo!")
+    print("weeeeeeeee")
+    print("I'm In!!")
+    print("hiii!")
+    print("blahahhahah")
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
+#yes
+
+##hi, we can also use this to chat

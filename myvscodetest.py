f085ae263462fd7c6a511717db29393c24b0795b
Shiva <shiva_vasudevan@utexas.edu>
2020-06-18 17:58:24 -0500

    committing links and more

--- myvscodetest.py
+++ myvscodetest.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import requests
+from requests_html import HTMLSession
+
 def main():
     
     print("Hello, World!")
@@ -14,6 +17,10 @@ def main():
     print("blahahhahah")
 >>>>>>> hello
 
+
+session = HTMLSession()
+r = session.get('https://python.org/')
+
 if __name__ == "__main__":
     # execute only if run as a script
     main()
@@ -21,3 +28,12 @@ if __name__ == "__main__":
 #yes
 
 ##hi, we can also use this to chat
+##anna, you better be sorry
+# do you guys want me to do anything in specific -Leglord
+# I can code java but my knowledge of python is still under development -Leglord
+#you can do whatever your heart tells you to do
+# lol im gonna write a script in python that just spits out a random naughty word every 5 seconds
+#looool, i gave access to my terminal to meethegeek and whatever earlier. i am probably gonna no longer grant access to my terminal to people
+#but you can do a python script, i'll run it for you
+#lol
+# naughty_words.py
---
+++ naughty_words.py
@@ -0,0 +1,62 @@
+import random
+x = input ('Type in a number between 1 and 10:')
+if x == 1:
+    print("poop")
+if x == 2:
+    print("fart")
+if x == 3:
+    print("doodoo")
+if x == 4:
+    print("pee")
+if x == 5:
+    print("")
+if x == 6:
+    print("")
+if x == 7:
+    print("")
+if x == 8:
+    print("")
+if x == 9:
+    print("")
+if x == 10:
+    print("")
+#@bearonionlol execute the script, i dare you
+# uhh... it doesnt work... I'm sticking to java
+# oh dang, this code doesn't work either->
+Package me.Leglord
+import java.lang.Math;
+public class Main(){
+    int x == math.random(0,1)*10;
+    switch (x) {
+case 1:
+    System.out.println("poop");
+    break;
+case 2:
+    System.out.println("doodoo");
+    break;
+case 3:
+    System.out.println("fart");
+    break;
+case 4:
+    System.out.println("pee");
+    break;
+case 5:
+    System.out.println("oo-laa-laa");
+    break;
+case 6:
+    System.out.println("curly brackets");
+    break;
+case 7:
+    System.out.println("cheese omelet");
+    break;
+case 8:
+    System.out.println("queso");
+    break;
+case 9:
+    System.out.println("fransuzich");
+    break;
+case 10:
+    System.out.println("the l-(u with umlaut)-ew-vre);
+    break;
+    }
+}
